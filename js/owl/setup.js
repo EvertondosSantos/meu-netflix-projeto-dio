905540baa4c9b5ea6952747d19a4e2f3c0fbc0a9
Everton <evertonsimoes1@hotmail.com>
2023-02-05 19:11:48 +0000

    c4 colocacao da rolagem dos filmes e conclusao do projeto

---
+++ js/owl/setup.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
